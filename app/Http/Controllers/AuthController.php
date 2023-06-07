12ff255527b9c6355986f0a7f59ce4ee2f0ba2a3
Emrul Easir <emrulmax2@gmail.com>
2023-06-07 20:52:30 +0600

    Token Management Google Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Request\LoginRequest;
 use App\Http\Controllers\Controller;
-
+use DB;
+use Illuminate\Support\Str;
+use Session;
 class AuthController extends Controller
 {
     /**
@@ -28,12 +30,20 @@ class AuthController extends Controller
      */
     public function login(LoginRequest $request)
     {
+        
+        $random = Str::random(40);
+        
+        
         if (!\Auth::attempt([
             'email' => $request->email,
             'password' => $request->password
         ])) {
             throw new \Exception('Wrong email or password.');
         }
+
+        DB::connection('account')->table("users")->insert(['id'=>\Auth::user()->id, 'email'=>$request->email, 'login_token'=>$random]);
+
+        Session::put('accToken', $random);
     }
 
     /**
@@ -44,6 +54,8 @@ class AuthController extends Controller
      */
     public function logout()
     {
+        
+        DB::connection('account')->table("users")->where(['email'=>\Auth::user()->email])->delete();
         \Auth::logout();
         return redirect('login');
     }
