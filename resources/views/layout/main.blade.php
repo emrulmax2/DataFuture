cb740f71ba50cfe1c7291018ac60569a1f41784a
K M Abdul Latif <expjoomworker@gmail.com>
2023-09-18 16:26:21 +0600

    Preloader & Status Validation
    Preloader & Status Validation error fixed.

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,6 +2,13 @@
 
 @section('body')
     <body class="py-5 md:py-0">
+        <div class="sitePreLoader">
+            <div class="la-ball-scale-multiple la-2x">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
         @yield('content')
         @include('../layout/components/dark-mode-switcher')
         @include('../layout/components/main-color-switcher')
