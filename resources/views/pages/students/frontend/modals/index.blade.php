9cd6817a002dbab60481bb41d6d7aad26b139843
Emrul Easir <emrulmax2@gmail.com>
2023-10-03 16:50:30 +0600

    modal added for student dashboard alert

---
+++ resources/views/pages/students/frontend/modals/index.blade.php
@@ -0,0 +1,20 @@
+<!-- BEGIN: Modal Content -->
+<div data-tw-backdrop="" aria-hidden="true" tabindex="-1" id="button-modal-preview" class="modal group bg-black/60 transition-[visibility,opacity] w-screen h-screen fixed left-0 top-0 [&amp;:not(.show)]:duration-[0s,0.2s] [&amp;:not(.show)]:delay-[0.2s,0s] [&amp;:not(.show)]:invisible [&amp;:not(.show)]:opacity-0 [&amp;.show]:visible [&amp;.show]:opacity-100 [&amp;.show]:duration-[0s,0.4s]">
+    <div class="w-[90%] mx-auto bg-white relative rounded-md shadow-md transition-[margin-top,transform] duration-[0.4s,0.3s] -mt-16 group-[.show]:mt-16 group-[.modal-static]:scale-[1.05] dark:bg-darkmode-600 sm:w-[460px]"><a class="absolute top-0 right-0 mt-3 mr-3" data-tw-dismiss="modal" href="#">
+            <i data-lucide="x" width="24" height="24" class="stroke-1.5 h-8 w-8 text-slate-400 h-8 w-8 text-slate-400"></i>
+
+
+        </a>
+        <div class="p-5 text-center">
+            <i data-lucide="alert-circle" width="24" height="24" class="stroke-1.5 mx-auto mt-3 h-16 w-16 text-success mx-auto mt-3 h-16 w-16 text-warning"></i>
+            <div class="mt-5 text-3xl">Need Information</div>
+            <div class="mt-2 text-slate-500">
+                we need some additional information to start your dashboard.
+            </div>
+        </div>
+        <div class="px-5 pb-8 text-center">
+            <button data-tw-merge data-tw-dismiss="modal" type="button" class="transition duration-200 border shadow-sm inline-flex items-center justify-center py-2 px-3 rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&amp;:hover:not(:disabled)]:bg-opacity-90 [&amp;:hover:not(:disabled)]:border-opacity-90 [&amp;:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed bg-primary border-primary text-white dark:border-primary w-24 w-24">Ok</button>
+        </div>
+    </div>
+</div>
+<!-- END: Modal Content -->
