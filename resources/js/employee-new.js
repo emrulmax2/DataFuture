dc443d4b3cfc1c06aa63a5bdda39b992e73b498a
Emrul Easir <emrulmax2@gmail.com>
2023-11-24 19:46:04 +0600

    update send

--- resources/js/employee-new.js
+++ resources/js/employee-new.js
@@ -226,6 +226,7 @@ import IMask from 'imask';
         let tthis = $(this)
 
         let typeText = $('option:selected',tthis).text();
+        console.log(typeText.match(/Employee/gi));
         if(typeText.match(/Employee/gi)!=null) {
             $('input[name="works_number"]').parent().removeClass('invisible')
             
