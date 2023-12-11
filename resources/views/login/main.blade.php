7f661fc47005b320da6e8b15fc50f2487a5a9e98
Emrul Easir <emrulmax2@gmail.com>
2023-12-11 15:36:08 +0600

    Update main.blade.php

--- resources/views/login/main.blade.php
+++ resources/views/login/main.blade.php
@@ -28,29 +28,29 @@
                     <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">Sign In</h2>
                     <div class="intro-x mt-2 text-slate-400 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your accounts in one place</div>
                     @if($env != "production") 
-                    <div class="intro-x mt-8">
-                        <form id="login-form">
-                            <input id="email" type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="Email" value="midone@left4code.com">
-                            <div id="error-email" class="login__input-error text-danger mt-2"></div>
-                            <input id="password" type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Password" value="password">
-                            <div id="error-password" class="login__input-error text-danger mt-2"></div>
-                        </form>
-                    </div>
-                    <div class="intro-x flex text-slate-600 dark:text-slate-500 text-xs sm:text-sm mt-4">
-                        <div class="flex items-center mr-auto">
-                            <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
-                            <label class="cursor-pointer select-none" for="remember-me">Remember me</label>
+                        <div class="intro-x mt-8">
+                            <form id="login-form">
+                                <input id="email" type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="Email" value="midone@left4code.com">
+                                <div id="error-email" class="login__input-error text-danger mt-2"></div>
+                                <input id="password" type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Password" value="password">
+                                <div id="error-password" class="login__input-error text-danger mt-2"></div>
+                            </form>
                         </div>
-                        <a href="">Forgot Password?</a>
-                    </div>
-                    <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-                        <button id="btn-login" class="btn btn-primary py-3 px-4 w-full md:w-64 xl:mr-3 align-top">Staff Login</button>
-                        <a href="{{ route('applicant.register') }}" class="transition duration-200 border shadow-sm inline-flex items-center justify-center rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&amp;:hover:not(:disabled)]:bg-opacity-90 [&amp;:hover:not(:disabled)]:border-opacity-90 [&amp;:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed border-secondary text-slate-500 dark:border-darkmode-100/40 dark:text-slate-300 [&amp;:hover:not(:disabled)]:bg-secondary/20 [&amp;:hover:not(:disabled)]:dark:bg-darkmode-100/10 mt-3 w-full px-4 py-3 align-top xl:mt-0 md:w-64">Register as applicant</a>
-                    </div>
+                        <div class="intro-x flex text-slate-600 dark:text-slate-500 text-xs sm:text-sm mt-4">
+                            <div class="flex items-center mr-auto">
+                                <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
+                                <label class="cursor-pointer select-none" for="remember-me">Remember me</label>
+                            </div>
+                            <a href="">Forgot Password?</a>
+                        </div>
+                        <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
+                            <button id="btn-login" class="btn btn-primary py-3 px-4 w-full md:w-64 xl:mr-3 align-top">Staff Login</button>
+                            <a href="{{ route('applicant.register') }}" class="transition duration-200 border shadow-sm inline-flex items-center justify-center rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&amp;:hover:not(:disabled)]:bg-opacity-90 [&amp;:hover:not(:disabled)]:border-opacity-90 [&amp;:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed border-secondary text-slate-500 dark:border-darkmode-100/40 dark:text-slate-300 [&amp;:hover:not(:disabled)]:bg-secondary/20 [&amp;:hover:not(:disabled)]:dark:bg-darkmode-100/10 mt-3 w-full px-4 py-3 align-top xl:mt-0 md:w-64">Register as applicant</a>
+                        </div>
 
-                    <div class="intro-x mt-4 text-center font-bold text-1xl">
-                         OR
-                    </div>
+                        <div class="intro-x mt-4 text-center font-bold text-1xl">
+                            OR
+                        </div>
                     @endif
                     <div class="flex items-center justify-center mt-5 intro-x  xl:mt-8 ">
                         <a href="{{ route('redirect.google') }}">
@@ -58,9 +58,9 @@
                         </a>
                     </div>
                     @if($env != "production") 
-                    <div class="intro-x mt-8 xl:mt-16 text-slate-600 dark:text-slate-500 text-center xl:text-left">
-                        By signin up, you agree to our <a class="text-primary dark:text-slate-200" href="">Terms and Conditions</a> & <a class="text-primary dark:text-slate-200" href="">Privacy Policy</a>
-                    </div>
+                        <div class="intro-x mt-8 xl:mt-16 text-slate-600 dark:text-slate-500 text-center xl:text-left">
+                            By signin up, you agree to our <a class="text-primary dark:text-slate-200" href="">Terms and Conditions</a> & <a class="text-primary dark:text-slate-200" href="">Privacy Policy</a>
+                        </div>
                     @endif
                 </div>
             </div>
