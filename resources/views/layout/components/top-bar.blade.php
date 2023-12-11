47ba4b1a21e4196126b79e900067db026de49a22
Emrul Easir <emrulmax2@gmail.com>
2023-12-11 20:20:25 +0600

    Update top-bar.blade.php

--- resources/views/layout/components/top-bar.blade.php
+++ resources/views/layout/components/top-bar.blade.php
@@ -1,17 +1,17 @@
 @php
 
 $employeeUser = cache()->get('employeeCache') ?? Auth::user()->load('employee'); 
-$opt = cache()->get('site_logo') ?? Option::where('category', 'SITE_SETTINGS')->where('name','site_logo')->pluck('value', 'name')->toArray()
+$opt = cache()->get('site_logo') ?? App\Models\Option::where('category', 'SITE_SETTINGS')->where('name','site_logo')->pluck('value', 'name')->toArray()
 @endphp
 <!-- BEGIN: Top Bar -->
 <div class="top-bar-boxed {{ isset($class) ? $class : '' }} h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] mt-12 md:mt-0 -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:fixed md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700">
     <div class="h-full flex items-center">
         <!-- BEGIN: Logo -->
         <a href="" class="logo -intro-x hidden md:flex xl:w-[180px] block">
-            <img alt="London Churchill College" class="logo__image w-6" src="{{ (isset($opt['site_logo']) && !empty($opt['site_logo']) && Storage::disk('local')->exists('public/'.$opt['site_logo']) ? Storage::disk('local')->url('public/'.$opt['site_logo']) : asset('build/assets/images/placeholders/200x200.jpg')) }}">
-            <span class="logo__text text-white text-lg ml-3">
+            <img alt="London Churchill College" class="logo__image w-24 h-10" src="{{ (isset($opt['site_logo']) && !empty($opt['site_logo']) && Storage::disk('local')->exists('public/'.$opt['site_logo']) ? Storage::disk('local')->url('public/'.$opt['site_logo']) : asset('build/assets/images/placeholders/200x200.jpg')) }}">
+            {{-- <span class="logo__text text-white text-lg ml-3">
                 Enigma
-            </span>
+            </span> --}}
         </a>
         <!-- END: Logo -->
         <!-- BEGIN: Breadcrumb -->
