929039386f9d5cdbfc4664c40f8cbfa33065cbf7
Emrul Easir <emrulmax2@gmail.com>
2024-03-20 22:53:09 +0600

    Update show.blade.php

--- resources/views/pages/interviewlist/profiles/show.blade.php
+++ resources/views/pages/interviewlist/profiles/show.blade.php
@@ -46,7 +46,7 @@
                 <div class="col-span-12 sm:col-span-3">
                     <div class="grid grid-cols-12 gap-0">
                         <div class="col-span-4 text-slate-500 font-medium">Gender</div>
-                        <div class="col-span-8 font-medium">{{ $applicant->gender }}</div>
+                        <div class="col-span-8 font-medium">{{ $applicant->sexid->name }}</div>
                     </div>
                 </div>
                 <div class="col-span-12 sm:col-span-3">
