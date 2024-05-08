205652c6f3086b7376182a1c146db0601b697987
K M Abdul Latif <expjoomworker@gmail.com>
2024-05-08 18:31:49 +0600

    File Manager Migration & Models

---
+++ app/Models/Document.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Document extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'document_info_id',
+        'doc_type',
+        'disk_type',
+        'path',
+        'display_file_name',
+        'current_file_name',
+        'expire_at',
+        'reminder_at',
+        
+        'created_by',
+        'updated_by',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+}
---
+++ app/Models/DocumentInfoHasEmployees.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentInfoHasEmployees extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'document_info_id',
+        'employee_id',
+        'document_role_and_permission_id'
+    ];
+}
