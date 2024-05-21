48571603b28342a9bbcc8181280202c08121d811
Emrul Easir <emrulmax2@gmail.com>
2024-05-21 15:10:17 +0600

    update agent-index

--- app/Http/Controllers/Agent/Frontend/DashboardController.php
+++ app/Http/Controllers/Agent/Frontend/DashboardController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers\Agent\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\AcademicYear;
+use App\Models\Agent;
 use App\Models\AgentApplicationCheck;
+use App\Models\AgentUser;
 use App\Models\Applicant;
+use App\Models\Course;
+use App\Models\CourseCreation;
+use App\Models\Semester;
+use App\Models\Status;
+use App\Models\TermDeclaration;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,25 +20,77 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        //$terms = TermDeclaration::orderBy('id','desc')->get();
         $userData = Auth::guard('agent')->user();
+        $agentUserList = AgentUser::where('id',$userData->id)->orWhere('parent_id',$userData->id)->get()->pluck('id')->toArray();
+        $agents = Agent::whereIn('agent_user_id',$agentUserList)->orderBy('first_name','ASC')->get();
         $data = AgentApplicationCheck::where('agent_user_id',$userData->id)->whereNull("applicant_id")->get();
         return view('pages.agent.dashboard.index', [
             'title' => 'Agent Dashboard - LCC Data Future Managment',
             'breadcrumbs' => [],
             'user' => $userData,
             'recentData' => $data,
-            'totalRecentApplication' => count($data)
+            'totalRecentApplication' => count($data),
+            'academicYears' => AcademicYear::all(),
+            'courses' => Course::orderBy('name','asc')->get(),
+            'semesters' => Semester::orderBy('id','desc')->get(),
+            'statuses' => Status::where('type','applicant')->get(),
+            'agents' =>$agents,
         ]);
     }
 
     public function list(Request $request){
+
+        $semesters = (isset($request->semesters) && !empty($request->semesters) ? $request->semesters : []);
+        $courses = (isset($request->courses) && !empty($request->courses) ? $request->courses : []);
+        $statuses = (isset($request->statuses) && !empty($request->statuses) ? $request->statuses : []);
+        $agents = (isset($request->agents) && !empty($request->agents) ? $request->agents : []);
+        $refno = (isset($request->refno) && !empty($request->refno) ? $request->refno : '');
+        $email = (isset($request->email) && !empty($request->email) ? $request->email : '');
+        $phone = (isset($request->phone) && !empty($request->phone) ? $request->phone : '');
+        $dob = (isset($request->dob) && !empty($request->dob) ? date('Y-m-d', strtotime($request->dob)) : '');
+
+        $courseCreationId = [];
+        if(!empty($courses)):
+            $courseCreations = CourseCreation::whereIn('course_id', $courses)->get();
+            if(!$courseCreations->isEmpty()):
+                foreach($courseCreations as $cc):
+                    $courseCreationId[] = $cc->id;
+                endforeach;
+            else:
+                $courseCreationId[1] = '0';
+            endif;
+        endif;
+
+
         $sorters = (isset($request->sorters) && !empty($request->sorters) ? $request->sorters : array(['field' => 'id', 'dir' => 'DESC']));
         $sorts = [];
         foreach($sorters as $sort):
             $sorts[] = $sort['field'].' '.$sort['dir'];
         endforeach;
         $userData = Auth::guard('agent')->user();
-        $query = Applicant::orderByRaw(implode(',', $sorts))->where('agent_user_id', $userData->id);
+        $query = Applicant::orderByRaw(implode(',', $sorts));
+  
+        if(count($agents)<=0) {
+
+            array_push($agents,$userData->id);
+
+        }
+
+        if(!empty($refno)): $query->where('application_no', $refno); endif;
+        if(!empty($email)): $query->where('email', $email); endif;
+        if(!empty($phone)): $query->where('phone', $phone); endif;
+        if(!empty($dob)): $query->where('date_of_birth', $dob); endif;
+        if(!empty($statuses)): $query->whereIn('status_id', $statuses); else: $query->where('status_id', '>', 1); endif;
+        if(!empty($semesters) || !empty($courseCreationId)):
+
+            $query->whereHas('course', function($qs) use($semesters, $courses, $courseCreationId){
+                if(!empty($semesters)): $qs->whereIn('semester_id', $semesters); endif;
+                if(!empty($courses) && !empty($courseCreationId)): $qs->whereIn('course_creation_id', $courseCreationId); endif;
+            });
+
+        endif;
+        if(!empty($agents)): $query->whereIn('agent_user_id', $agents); endif;
 
         $total_rows = $query->count();
         $page = (isset($request->page) && $request->page > 0 ? $request->page : 0);
@@ -50,7 +110,7 @@ class DashboardController extends Controller
             $i = 1;
             foreach($Query as $list):
                 
-                $agentCheck = AgentApplicationCheck::where('agent_user_id',$userData->id)->where("email",$list->users->email)->where("mobile",$list->users->phone)->get()->first();
+                $agentCheck = AgentApplicationCheck::whereIn('agent_user_id',$agents)->where("email",$list->users->email)->where("mobile",$list->users->phone)->get()->first();
                 $data[] = [
                     'id' => $list->id,
                     'sl' => $i,
@@ -60,7 +120,8 @@ class DashboardController extends Controller
                     'applicationCheck' => isset($agentCheck->id) && !empty($agentCheck->id) ? $agentCheck->id : '',
                     'gender' => isset($list->sexid->name) && !empty($list->sexid->name) ? $list->sexid->name : '',
                     'course' => (isset($list->course->creation->course->name) ? $list->course->creation->course->name : '').(isset($list->course->semester->name) ? ' - '.$list->course->semester->name : ''),
-                    'submission_date' => $list->submission_date . "( ".$list->referral_code." )",
+                    'submission_date' => $list->submission_date,
+                    'referral_code' => $list->referral_code,
                     'status' => (!empty($list->submission_date) ? (isset($list->status->name) ? $list->status->name : 'Unknown') : 'Incomplete'),
                     'deleted_at' => $list->deleted_at
                 ];
--- resources/views/pages/agent/dashboard/index.blade.php
+++ resources/views/pages/agent/dashboard/index.blade.php
@@ -131,10 +131,107 @@
                 </div>
                 @php session()->forget('applicantSubmission'); @endphp
             @endif
+
+            <div id="studentSearchAccordionWrap" class="pt-4 mb-2">
+    
+                <div id="studentSearchAccordion" class="accordion accordion-boxed pt-2">
+                    <div class="accordion-item">
+                        <div id="studentSearchAccordion-1" class="accordion-header">
+                            <button  id="studentGroupSearchBtn" class="accordion-button collapsed relative w-full text-sm font-semibold" type="button" data-tw-toggle="collapse" data-tw-target="#studentSearchAccordion-collapse-1" aria-expanded="false" aria-controls="studentSearchAccordion-collapse-1">
+                                Search
+                                <span class="accordionCollaps" style="width: 18px; height: 16px;"></span>
+                            </button>
+                        </div>
+                        <div id="studentSearchAccordion-collapse-1" class="accordion-collapse collapse" aria-labelledby="studentSearchAccordion-1" data-tw-parent="#studentSearchAccordion">
+                            <div class="accordion-body">
+                                <div class="grid grid-cols-12 gap-0 gap-y-2 gap-x-4">
+                                    <div class="col-span-12 sm:col-span-3">
+                                        <label for="application_no" class="form-label">Referrence No</label>
+                                        <div class="autoCompleteField" data-table="students">
+                                            <input type="text" autocomplete="off" id="application_no" name="application_no" class="form-control registration_no" value="" placeholder=""/>
+                                            <ul class="autoFillDropdown"></ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-span-12 sm:col-span-3">
+                                        <label for="applicantEmail" class="form-label">Applicant Email</label>
+                                        <div class="autoCompleteField" data-table="students">
+                                            <input type="text" autocomplete="off" id="applicantEmail" name="applicantEmail" class="form-control email" value="" placeholder=""/>
+                                            <ul class="autoFillDropdown"></ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-span-12 sm:col-span-3">
+                                        <label for="phone" class="form-label">Applicant Phone</label>
+                                        <div class="autoCompleteField" data-table="students">
+                                            <input type="text" autocomplete="off" id="applicantPhone" name="applicantPhone" class="form-control phone" value="" placeholder=""/>
+                                            <ul class="autoFillDropdown"></ul>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="col-span-12 sm:col-span-3">
+                                        <label for="semesters" class="form-label">Intake Semester</label>
+                                        <select id="semesters" class="w-full tom-selects" name="semesters">
+                                            <option value="">Please Select</option>
+                                            @if(!empty($semesters))
+                                                @foreach($semesters as $sem)
+                                                    <option value="{{ $sem->id }}">{{ $sem->name }}</option>
+                                                @endforeach
+                                            @endif
+                                        </select>
+                                        <div class="acc__input-error error-intake_semester text-danger mt-2"></div>
+                                    </div>
+                                    <div class="col-span-12 sm:col-span-3">
+                                        <label for="courses" class="form-label">Course </label>
+                                        <select id="courses" class="w-full tom-selects" name="courses">
+                                            <option value="">Please Select</option>
+                                            @if(!empty($courses))
+                                                @foreach($courses as $crs)
+                                                    <option value="{{ $crs->id }}">{{ $crs->name }}</option>
+                                                @endforeach
+                                            @endif
+                                        </select>
+                                        <div class="acc__input-error error-course text-danger mt-2"></div>
+                                    </div>
+                                    <div class="col-span-12 sm:col-span-3">
+                                        <label for="statuses" class="form-label">Status</label>
+                                        <select id="statuses" class="w-full tom-selects" name="statuses[]" multiple>
+                                            <option value="">Please Select</option>
+                                            @if(!empty($statuses))
+                                            @foreach($statuses as $crs)
+                                                <option value="{{ $crs->id }}">{{ $crs->name }}</option>
+                                            @endforeach
+                                        @endif
+                                        </select>
+                                    </div>
+                                    
+                                    <div class="col-span-12 sm:col-span-3">
+                                        <label for="agents" class="form-label">Agent/SubAgents</label>
+                                        <select id="agents" class="w-full tom-selects" name="agents[]" multiple>
+                                            <option value="">Please Select</option>
+                                            @if(!empty($agents))
+                                            @foreach($agents as $crs)
+                                                <option value="{{ $crs->id }}">{{ $crs->full_name }} [{{ $crs->code }}]</option>
+                                            @endforeach
+                                        @endif
+                                        </select>
+                                    </div>
+
+                                    
+                                    <div class="col-span-12 sm:col-span-6 pt-7">
+                                        <button id="studentGroupSearchSubmitBtn" type="button" class="btn btn-success text-white ml-2 w-auto"><i class="w-4 h-4 mr-2" data-lucide="search"></i> Search <i  data-loading-icon="oval" data-color="white" class="w-4 h-4 ml-2 searchLoading hidden" ></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="overflow-x-auto scrollbar-hidden">
                 <div id="applicantApplicantionList" class="mt-5 table-report table-report--tabulator"></div>
             </div>
+
         </div>
+
+        
         <div class="col-span-12 xl:col-span-3 2xl:col-span-3">
             <div class="2xl:border-l -mb-10 pb-10">
                 <div class="2xl:pl-6 grid grid-cols-12 gap-x-6 2xl:gap-x-0 gap-y-6">
