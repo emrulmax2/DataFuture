ef6582ed7685386f84222b31c8565bc36592437f
K M Abdul Latif <expjoomworker@gmail.com>
2024-05-23 22:33:48 +0600

    HR Fixing.

--- resources/js/employee-appraisal-documents.js
+++ resources/js/employee-appraisal-documents.js
@@ -73,7 +73,9 @@ var employeeAppraisalDocListTable = (function () {
                     download: false,
                     formatter(cell, formatterParams) {                        
                         var btns = "";
-                        btns +='<a target="_blank" href="'+cell.getData().url+'" download class="btn-rounded btn btn-linkedin text-white p-0 w-9 h-9 ml-1"><i data-lucide="cloud-lightning" class="w-4 h-4"></i></a>';
+                        if(cell.getData().url == 1){
+                            btns +='<a data-id="' + cell.getData().id + '" href="javascript:void(0);" class="downloadDoc btn-rounded btn btn-linkedin text-white p-0 w-9 h-9 ml-1"><i data-lucide="cloud-lightning" class="w-4 h-4"></i></a>';
+                        }
                         if (cell.getData().deleted_at == null) {
                             btns += '<button data-id="' + cell.getData().id + '"  class="delete_btn btn btn-danger text-white btn-rounded ml-1 p-0 w-9 h-9"><i data-lucide="Trash2" class="w-4 h-4"></i></button>';
                         }else if(cell.getData().deleted_at != null) {
@@ -417,5 +419,34 @@ var employeeAppraisalDocListTable = (function () {
         }else{
             confirmModal.hide();
         }
+    });
+
+    $('#employeeAppraisalDocListTable').on('click', '.downloadDoc', function(e){
+        e.preventDefault();
+        var $theLink = $(this);
+        var row_id = $theLink.attr('data-id');
+
+        $theLink.css({'opacity' : '.6', 'cursor' : 'not-allowed'});
+
+        axios({
+            method: "post",
+            url: route('employee.documents.download.url'),
+            data: {row_id : row_id},
+            headers: {'X-CSRF-TOKEN' :  $('meta[name="csrf-token"]').attr('content')},
+        }).then(response => {
+            if (response.status == 200){
+                let res = response.data.res;
+                $theLink.css({'opacity' : '1', 'cursor' : 'pointer'});
+
+                if(res != ''){
+                    window.open(res, '_blank');
+                }
+            } 
+        }).catch(error => {
+            if(error.response){
+                $theLink.css({'opacity' : '1', 'cursor' : 'pointer'});
+                console.log('error');
+            }
+        });
     });
 })()
