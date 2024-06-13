a965254f0b25f93c891fc183638c07d93c79b610
K M Abdul Latif <expjoomworker@gmail.com>
2024-06-13 17:32:44 +0600

    Punch Modifications

--- resources/js/machine-live.js
+++ resources/js/machine-live.js
@@ -2,6 +2,80 @@ import { createIcons, icons } from "lucide";
 
 
 (function(){
+    const clockoutConfirmModal = tailwind.Modal.getOrCreateInstance(document.querySelector("#clockoutConfirmModal"));
+    const successModal = tailwind.Modal.getOrCreateInstance(document.querySelector("#successModal"));
+    const warningModal = tailwind.Modal.getOrCreateInstance(document.querySelector("#warningModal"));
+
+    
+    const clockoutConfirmModalEl = document.getElementById('clockoutConfirmModal')
+    clockoutConfirmModalEl.addEventListener('hide.tw.modal', function(event) {
+        $('#clockoutConfirmModal .employeeName').html('');
+        $('#clockoutConfirmModal button').attr('data-clockinno', clock_in_no);
+
+        $('#clockoutConfirmModal button').removeAttr('disabled');
+        $('#clockoutConfirmModal button').find('svg').fadeOut();
+    });
+
+    $('#clockoutConfirmModal button').on('click', function(e){
+        e.preventDefault();
+        let $theBtn = $(this);
+        let clockinno = $theBtn.attr('data-clockinno');
+        let type = $theBtn.attr('data-type');
+
+        $('#clockoutConfirmModal button').attr('disabled', 'disabled');
+        $theBtn.find('svg').fadeIn();
+
+        axios({
+            method: "post",
+            url: route('attendance.punch.store.dif'),
+            data: {clockinno : clockinno, type : type},
+            headers: {'X-CSRF-TOKEN' :  $('meta[name="csrf-token"]').attr('content')},
+        }).then(response => {
+            clockoutConfirmModal.hide();
+            $('#liveAttendanceForm').find('.theMessage').remove();
+            $('#liveAttendanceForm').find('svg').css({display : 'none'});
+            $('#liveAttendanceForm .btn-action').fadeOut().attr('disabled', 'disabled');
+            $('#liveAttendanceForm').find('[name="attendance_type"]').val('0');
+            $('#liveAttendanceForm').find('#clock_in_no').val('');
+
+            if (response.status == 200) {
+                let res = response.data.res;
+                
+                if(res.suc == 2){
+                    $('#liveAttendanceForm').find('.theMessage').remove();
+                    $('#liveAttendanceForm').prepend('<div class="text-white alert alert-warning theMessage show flex items-start mb-3 text-lg font-medium" role="alert"><i data-lucide="alert-octagon" class="w-6 h-6 mr-2"></i><span>'+res.msg+'</span></div>')
+                    createIcons({ icons, "stroke-width": 1.5, nameAttr: "data-lucide", });
+                }else{
+                    $('#liveAttendanceForm').find('.theMessage').remove();
+                    $('#liveAttendanceForm').prepend('<div class="text-white alert alert-success theMessage show flex items-center mb-3 text-lg font-medium" role="alert"><i data-lucide="alert-octagon" class="w-6 h-6 mr-2"></i><span>'+res.msg+'</span></div>')
+                    createIcons({ icons, "stroke-width": 1.5, nameAttr: "data-lucide", });
+                }
+
+                setTimeout(function(){
+                    $('#liveAttendanceForm').find('.theMessage').fadeOut().remove();
+                }, 2000);
+            }
+            
+        }).catch(error => {
+            if(error.response){
+                if(error.response.status == 422){
+                    console.log('error');
+                }
+            }
+        });
+    });
+
+    $('#liveAttendanceForm .btn-action.btn-type-4').on('click', function(e){
+        e.preventDefault();
+        let $theBtn = $(this);
+        let employee = $theBtn.attr('data-employee');
+        let clock_in_no = $('#clock_in_no').val();
+        clockoutConfirmModal.show();
+
+        $('#clockoutConfirmModal .employeeName').html(employee);
+        $('#clockoutConfirmModal button').attr('data-clockinno', clock_in_no);
+    });
+
     $('#liveAttendanceForm input[name="clock_in_no"]').on('keypress', function(event){
         if(event.which == 13){
             
@@ -9,7 +83,7 @@ import { createIcons, icons } from "lucide";
             let clock_in_no = $form.find('[name="clock_in_no"]').val();
             var $buttonGroup = $form.find('.liveAttendanceFormBtnGroup');
             var $actionButtons = $buttonGroup.find('.btn-action');
-            var $backButton = $buttonGroup.find('.btn-back');
+            //var $backButton = $buttonGroup.find('.btn-back');
             var clockinno = $form.find('#clock_in_no').val();
             if(clock_in_no != ''){
                 axios({
@@ -28,7 +102,7 @@ import { createIcons, icons } from "lucide";
 
                                 $actionButtons.fadeOut().attr('disabled', 'disabled');
                                 $buttonGroup.find('.btn-action.btn-type-1').fadeIn().removeAttr('disabled');
-                                $backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
+                                //$backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
                             });
 
                             $form.addClass('activeForm');
@@ -40,8 +114,8 @@ import { createIcons, icons } from "lucide";
 
                                 $actionButtons.fadeOut().attr('disabled', 'disabled');
                                 $buttonGroup.find('.btn-action.btn-type-2').fadeIn().removeAttr('disabled');
-                                $buttonGroup.find('.btn-action.btn-type-4').fadeIn().removeAttr('disabled');
-                                $backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
+                                $buttonGroup.find('.btn-action.btn-type-4').fadeIn().removeAttr('disabled').attr('data-employee', res.name);
+                                //$backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
                             });
 
                             $form.addClass('activeForm');
@@ -53,7 +127,7 @@ import { createIcons, icons } from "lucide";
 
                                 $actionButtons.fadeOut().attr('disabled', 'disabled');
                                 $buttonGroup.find('.btn-action.btn-type-3').fadeIn().removeAttr('disabled');
-                                $backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
+                                //$backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
                             });
 
                             $form.addClass('activeForm');
@@ -65,8 +139,8 @@ import { createIcons, icons } from "lucide";
 
                                 $actionButtons.fadeOut().attr('disabled', 'disabled');
                                 $buttonGroup.find('.btn-action.btn-type-2').fadeIn().removeAttr('disabled');
-                                $buttonGroup.find('.btn-action.btn-type-4').fadeIn().removeAttr('disabled');
-                                $backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
+                                $buttonGroup.find('.btn-action.btn-type-4').fadeIn().removeAttr('disabled').attr('data-employee', res.name);
+                                //$backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
                             });
 
                             $form.addClass('activeForm');
@@ -78,7 +152,7 @@ import { createIcons, icons } from "lucide";
 
                                 $actionButtons.fadeOut().attr('disabled', 'disabled');
                                 //$buttonGroup.find('.btn-action.btn-type-1').fadeIn().removeAttr('disabled');
-                                $backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
+                                //$backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
                             });
 
                             $form.addClass('activeForm');
@@ -89,7 +163,7 @@ import { createIcons, icons } from "lucide";
 
                             $buttonGroup.fadeIn();
                             $actionButtons.attr('disabled', 'disabled');
-                            $backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
+                            //$backButton.css({ display: 'inline-flex'}).removeAttr('disabled');
                         }
 
                         setTimeout(function(){
@@ -104,7 +178,7 @@ import { createIcons, icons } from "lucide";
                                 });
                                 $form.removeClass('activeForm');
                             }
-                        }, 40000);
+                        }, 10000);
                     }
                     
                 }).catch(error => {
@@ -144,10 +218,10 @@ import { createIcons, icons } from "lucide";
         let $form = $(this);
         let attendance_type = $form.find('[name="attendance_type"]').val();
         let $submitBtn = $form.find('.btn-type-'+attendance_type);
-        let $backBtn = $form.find('.btn-back');
+        //let $backBtn = $form.find('.btn-back');
         let $actopmBtn = $form.find('.btn-action');
 
-        $backBtn.attr('disabled', 'disabled');
+        //$backBtn.attr('disabled', 'disabled');
         $submitBtn.attr('disabled', 'disabled');
         $submitBtn.find('svg').css({display : 'inline-block'})
 
@@ -158,7 +232,7 @@ import { createIcons, icons } from "lucide";
             data: form_data,
             headers: {'X-CSRF-TOKEN' :  $('meta[name="csrf-token"]').attr('content')},
         }).then(response => {
-            $backBtn.removeAttr('disabled', 'disabled');
+            //$backBtn.removeAttr('disabled', 'disabled');
             $submitBtn.find('svg').css({display : 'none'});
             $actopmBtn.fadeOut().attr('disabled', 'disabled');
             $form.find('[name="attendance_type"]').val('0');
@@ -178,14 +252,12 @@ import { createIcons, icons } from "lucide";
                 }
 
                 setTimeout(function(){
-                    $backBtn.fadeOut().attr('disabled', 'disabled').fadeOut(function(){
-                        $form.find('.theMessage').fadeOut().remove();
-                    });
+                    $form.find('.theMessage').fadeOut().remove();
                 }, 2000);
             }
             
         }).catch(error => {
-            $backBtn.removeAttr('disabled', 'disabled');
+            //$backBtn.removeAttr('disabled', 'disabled');
             $submitBtn.find('svg').css({display : 'none'});
             $actopmBtn.fadeOut().attr('disabled', 'disabled');
             $form.find('[name="attendance_type"]').val('0');
--- resources/views/pages/hr/punch/index.blade.php
+++ resources/views/pages/hr/punch/index.blade.php
@@ -53,6 +53,21 @@
                                         </g>
                                     </svg>
                                 </button>
+                                <button type="button" data-employee="" data-value="4" disabled class="btn-type-4 btn btn-danger btn-action">
+                                    Clock Out
+                                    <svg style="display: none;" width="25" viewBox="-2 -2 42 42" xmlns="http://www.w3.org/2000/svg"
+                                        stroke="white" class="w-4 h-4 ml-2">
+                                        <g fill="none" fill-rule="evenodd">
+                                            <g transform="translate(1 1)" stroke-width="4">
+                                                <circle stroke-opacity=".5" cx="18" cy="18" r="18"></circle>
+                                                <path d="M36 18c0-9.94-8.06-18-18-18">
+                                                    <animateTransform attributeName="transform" type="rotate" from="0 18 18"
+                                                        to="360 18 18" dur="1s" repeatCount="indefinite"></animateTransform>
+                                                </path>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </button>
                                 <button type="submit" value="2" disabled class="btn-type-2 btn btn-twitter btn-action" onclick="this.form.attendance_type.value = this.value">
                                     Break
                                     <svg style="display: none;" width="25" viewBox="-2 -2 42 42" xmlns="http://www.w3.org/2000/svg"
@@ -83,24 +98,9 @@
                                         </g>
                                     </svg>
                                 </button>
-                                <button type="submit" value="4" disabled class="btn-type-4 btn btn-danger btn-action" onclick="this.form.attendance_type.value = this.value">
-                                    Clock Out
-                                    <svg style="display: none;" width="25" viewBox="-2 -2 42 42" xmlns="http://www.w3.org/2000/svg"
-                                        stroke="white" class="w-4 h-4 ml-2">
-                                        <g fill="none" fill-rule="evenodd">
-                                            <g transform="translate(1 1)" stroke-width="4">
-                                                <circle stroke-opacity=".5" cx="18" cy="18" r="18"></circle>
-                                                <path d="M36 18c0-9.94-8.06-18-18-18">
-                                                    <animateTransform attributeName="transform" type="rotate" from="0 18 18"
-                                                        to="360 18 18" dur="1s" repeatCount="indefinite"></animateTransform>
-                                                </path>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                </button>
-                                <button disabled disabled class="btn btn-warning text-white btn-back">
+                                {{--<button disabled disabled class="btn btn-warning text-white btn-back">
                                     Back
-                                </button>
+                                </button>--}}
                             </div>
                         </div>
                     </div>
@@ -114,6 +114,97 @@
             </div>
         </div>
     </div>
+
+    <!-- BEGIN: Clockout Confirm Modal Content -->
+    <div id="clockoutConfirmModal" class="modal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body p-0">
+                    <div class="p-5 text-center">
+                        <i data-lucide="alert-octagon" class="w-16 h-16 mx-auto text-danger mt-3"></i>
+                        <div class="text-3xl mt-5 confModTitle">Hi <span class="employeeName"></span></div>
+                        <div class="text-slate-500 mt-2 confModDesc">
+                            <p class="pb-2">
+                                Are you sure you want to clock out? Once you do, you won't be able to clock back in today. Clocking out means you're leaving work for today.
+                            </p>
+                            <p class="pb-2">
+                                If you're just going on a break, please click 
+                                <button type="button" data-clockinno="" data-type="2" class="btn btn-twitter w-auto mr-1">
+                                    Break
+                                    <svg style="display: none;" width="25" viewBox="-2 -2 42 42" xmlns="http://www.w3.org/2000/svg"
+                                        stroke="white" class="w-4 h-4 ml-2">
+                                        <g fill="none" fill-rule="evenodd">
+                                            <g transform="translate(1 1)" stroke-width="4">
+                                                <circle stroke-opacity=".5" cx="18" cy="18" r="18"></circle>
+                                                <path d="M36 18c0-9.94-8.06-18-18-18">
+                                                    <animateTransform attributeName="transform" type="rotate" from="0 18 18"
+                                                        to="360 18 18" dur="1s" repeatCount="indefinite"></animateTransform>
+                                                </path>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </button>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="px-5 pb-8 text-center">
+                        <button type="button" data-clockinno="" data-type="4" data-action="none" class="btn btn-danger w-auto">
+                            Confirm Clock Out
+                            <svg style="display: none;" width="25" viewBox="-2 -2 42 42" xmlns="http://www.w3.org/2000/svg"
+                                stroke="white" class="w-4 h-4 ml-2">
+                                <g fill="none" fill-rule="evenodd">
+                                    <g transform="translate(1 1)" stroke-width="4">
+                                        <circle stroke-opacity=".5" cx="18" cy="18" r="18"></circle>
+                                        <path d="M36 18c0-9.94-8.06-18-18-18">
+                                            <animateTransform attributeName="transform" type="rotate" from="0 18 18"
+                                                to="360 18 18" dur="1s" repeatCount="indefinite"></animateTransform>
+                                        </path>
+                                    </g>
+                                </g>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END: Clockout Confirm Modal Content -->
+    <!-- BEGIN: Success Modal Content -->
+    <div id="successModal" class="modal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body p-0">
+                    <div class="p-5 text-center">
+                        <i data-lucide="check-circle" class="w-16 h-16 text-success mx-auto mt-3"></i>
+                        <div class="text-3xl mt-5 successModalTitle"></div>
+                        <div class="text-slate-500 mt-2 successModalDesc"></div>
+                    </div>
+                    <div class="px-5 pb-8 text-center">
+                        <button type="button" data-tw-dismiss="modal" class="btn btn-primary w-24">Ok</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END: Success Modal Content -->
+    <!-- BEGIN: Success Modal Content -->
+    <div id="warningModal" class="modal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body p-0">
+                    <div class="p-5 text-center">
+                        <i data-lucide="alert-octagon" class="w-16 h-16 text-success mx-auto mt-3"></i>
+                        <div class="text-3xl mt-5 warningModalTitle"></div>
+                        <div class="text-slate-500 mt-2 warningModalDesc"></div>
+                    </div>
+                    <div class="px-5 pb-8 text-center">
+                        <button type="button" data-tw-dismiss="modal" class="btn btn-primary w-24">Ok</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END: Success Modal Content -->
 @endsection
 
 @section('script')
