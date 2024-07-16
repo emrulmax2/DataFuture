5fc0f88a67014efb3f276c49c05ef44eaf9ee79e
Emrul Easir <emrulmax2@gmail.com>
2024-07-16 16:29:44 +0600

    Update DashboardController.php

--- app/Http/Controllers/Agent/Frontend/DashboardController.php
+++ app/Http/Controllers/Agent/Frontend/DashboardController.php
@@ -105,7 +105,7 @@ class DashboardController extends Controller
         endif;
         if(!empty($agents)): $query->whereIn('agent_user_id', $agents); endif;
 
-        $total_rows = 1;//$query->count();
+        $total_rows = $query->count();
         $page = (isset($request->page) && $request->page > 0 ? $request->page : 0);
         $perpage = (isset($request->size) && $request->size == 'true' ? $total_rows : ($request->size > 0 ? $request->size : 10));
         $last_page = $total_rows > 0 ? ceil($total_rows / $perpage) : '';
