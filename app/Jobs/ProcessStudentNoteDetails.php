979a9a5c8b306f75c9d13309401ad057770c656d
Emrul Easir <emrulmax2@gmail.com>
2024-09-02 21:12:56 +0600

    Update ProcessStudentNoteDetails.php

--- app/Jobs/ProcessStudentNoteDetails.php
+++ app/Jobs/ProcessStudentNoteDetails.php
@@ -48,45 +48,45 @@ class ProcessStudentNoteDetails implements ShouldQueue
         $user = StudentUser::where(["email"=> $ApplicantUser->email])->get()->first();
         $student = Student::where(["student_user_id"=> $user->id])->get()->first(); 
 
-        foreach ($this->applicant->notes as $note):
+        // foreach ($this->applicant->notes as $note):
             
-            $note = ApplicantNote::find($note->id);
+        //     $note = ApplicantNote::find($note->id);
 
-            $studentDocument = new StudentDocument();
-            //DB::enableQueryLog();
+        //     $studentDocument = new StudentDocument();
+        //     //DB::enableQueryLog();
 
-            $applicantArray = [
-                'student_id' => $student->id,
-                'hard_copy_check' => $note->document->hard_copy_check,
-                'doc_type' => $note->document->doc_type,
-                'disk_type' => $note->document->disk_type,
-                'path' => $note->document->path,
-                'display_file_name' =>	 $note->document->display_file_name,
-                'current_file_name' => $note->document->current_file_name,
-                'created_by'=> ($note->document->updated_by) ? $note->document->updated_by : $note->document->created_by,
-            ];
-            if($note->document->document_setting_id) {
-                $applicantArray = array_merge($applicantArray,['document_setting_id' => $note->document->document_setting_id]);
-            }
-            $studentDocument->fill($applicantArray);
+        //     $applicantArray = [
+        //         'student_id' => $student->id,
+        //         'hard_copy_check' => $note->document->hard_copy_check,
+        //         'doc_type' => $note->document->doc_type,
+        //         'disk_type' => $note->document->disk_type,
+        //         'path' => $note->document->path,
+        //         'display_file_name' =>	 $note->document->display_file_name,
+        //         'current_file_name' => $note->document->current_file_name,
+        //         'created_by'=> ($note->document->updated_by) ? $note->document->updated_by : $note->document->created_by,
+        //     ];
+        //     if($note->document->document_setting_id) {
+        //         $applicantArray = array_merge($applicantArray,['document_setting_id' => $note->document->document_setting_id]);
+        //     }
+        //     $studentDocument->fill($applicantArray);
 
-            $studentDocument->save();
-            //$queries = DB::getQueryLog();
+        //     $studentDocument->save();
+        //     //$queries = DB::getQueryLog();
 
-            // Log::debug($queries);
+        //     // Log::debug($queries);
 
-            unset ($applicantArray);
-            $studentNote = new StudentNote();
-            $applicantArray = [
-                'student_id' => $student->id,
-                'student_document_id' => $studentDocument->id,
-                'note' => $note->note,
-                'phase'=> 'Live',
-                'created_by'=> ($this->applicant->updated_by) ? $this->applicant->updated_by : $this->applicant->created_by,
-            ];
-            $studentNote->fill($applicantArray);
-            $studentNote->save();
-            unset ($applicantArray);
-        endforeach;
+        //     unset ($applicantArray);
+        //     $studentNote = new StudentNote();
+        //     $applicantArray = [
+        //         'student_id' => $student->id,
+        //         'student_document_id' => $studentDocument->id,
+        //         'note' => $note->note,
+        //         'phase'=> 'Live',
+        //         'created_by'=> ($this->applicant->updated_by) ? $this->applicant->updated_by : $this->applicant->created_by,
+        //     ];
+        //     $studentNote->fill($applicantArray);
+        //     $studentNote->save();
+        //     unset ($applicantArray);
+        // endforeach;
     }
 }
