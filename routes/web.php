eb70898f867f4c266a629fe8dac6ce53ce475c65
Emrul Easir <emrulmax2@gmail.com>
2024-09-13 14:25:29 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -693,6 +693,7 @@ Route::middleware('auth')->group(function() {
     });
 
     Route::controller(StudentController::class)->group(function() {
+        
         Route::get('student', 'index')->name('student'); 
         Route::get('student/list', 'list')->name('student.list'); 
         Route::get('student/groupsearch-list', 'groupSearchList')->name('student.list.by.groupsearch'); 
@@ -726,6 +727,7 @@ Route::middleware('auth')->group(function() {
         Route::post('student/all-groups', 'getAllGroups')->name('student.get.groups');
         
         Route::post('student/temm-by-academic', 'getAllTerms')->name('student.get.term.by.academics');
+        Route::post('student/get-student-type', 'getAllStudentType')->name('student.get.all.student.type');
         Route::post('student/course-by-terms', 'getAllCourses')->name('student.get.coureses.by.terms');
         Route::post('student/status-by-groups', 'getAllStatuses')->name('student.get.status.by.groups');
 
