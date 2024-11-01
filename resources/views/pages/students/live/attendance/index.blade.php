3d8221db406b0ad2d3a73de72f2d4ac632aac3e5
Emrul Easir <emrulmax2@gmail.com>
2024-11-01 18:11:09 +0600

    Update index.blade.php

--- resources/views/pages/students/live/attendance/index.blade.php
+++ resources/views/pages/students/live/attendance/index.blade.php
@@ -68,7 +68,8 @@
             </div>
             <div class="w-full py-3  {{ ($attendanceIndicator[$termId]===0 ? "border-red-600" : "border-teal-600" ) }} border-2 rounded-b-lg bg-transparent h-full">
                 @foreach($dataStartPoint as $planId => $data)
-                
+                    @if(!isset($planDetails[$termId][$planId]))
+                        @continue
                     <div class="p-5 ">
 
                         <div class="relative flex items-center mb-5">
