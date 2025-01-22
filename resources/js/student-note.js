63bbc97c66010bf4f08217091f51119c9b674133
K M Abdul Latif <143424592+kmabdullatif@users.noreply.github.com>
2025-01-22 16:29:50 +0600

    Student Follow UP Modification Done

--- resources/js/student-note.js
+++ resources/js/student-note.js
@@ -3,6 +3,7 @@ import xlsx from "xlsx";
 import { createIcons, icons } from "lucide";
 import Tabulator from "tabulator-tables";
 import TomSelect from "tom-select";
+import tippy, { roundArrow } from "tippy.js";
 
 ("use strict");
 var studentNotesListTable = (function () {
@@ -128,13 +129,20 @@ var studentNotesListTable = (function () {
                     headerSort: false,
                     hozAlign: "right",
                     headerHozAlign: "right",
-                    width: "120",
+                    width: "220",
                     download: false,
                     formatter(cell, formatterParams) {                        
                         var btns = "";
                         //if(cell.getData().note_document_id > 0){
                             //btns +='<a data-id="'+cell.getData().note_document_id+'" href="javascript:void(0);" class="downloadDoc btn-rounded btn btn-linkedin text-white p-0 w-9 h-9 ml-1"><i data-lucide="cloud-lightning" class="w-4 h-4"></i></a>';
                         //}
+                        if(cell.getData().followed_up == 'yes' && cell.getData().am_i_followed){
+                            var countHtml = cell.getData().unread_comment > 0 ? '<span class="bg-danger absolute r-0 t-0" style="    width: 18px; height: 18px; border-radius: 50%; font-size: 11px; line-height: 1; padding: 3px 0 0; margin: -5px -5px 0 0;">'+cell.getData().unread_comment+'</span>' : '';
+                            btns += '<button data-id="' + cell.getData().id + '" data-tw-toggle="modal" data-tw-target="#followUpCommentModal"  class="viewCommentBtn relative btn btn-twitter text-white btn-rounded ml-1 p-0 w-9 h-9"><i data-lucide="message-square-plus" class="w-4 h-4"></i>'+countHtml+'</button>';
+                            if(cell.getData().followed_up_status == 'Pending'){
+                                btns += '<button data-id="' + cell.getData().id + '" type="button" class="completedBtn btn-rounded btn btn-success text-white p-0 w-9 h-9 ml-1"><i data-lucide="check-circle" class="w-4 h-4"></i></a>';
+                            }
+                        }
                         if (cell.getData().deleted_at == null) {
                             //btns += '<button data-id="' + cell.getData().id + '" data-tw-toggle="modal" data-tw-target="#viewNoteModal"  class="view_btn btn btn-twitter text-white btn-rounded ml-1 p-0 w-9 h-9"><i data-lucide="eye-off" class="w-4 h-4"></i></button>';
                             if(cell.getData().is_ownere == 1){
@@ -263,6 +271,7 @@ var studentNotesListTable = (function () {
     const addNoteModal = tailwind.Modal.getOrCreateInstance(document.querySelector("#addNoteModal"));
     const viewNoteModal = tailwind.Modal.getOrCreateInstance(document.querySelector("#viewNoteModal"));
     const editNoteModal = tailwind.Modal.getOrCreateInstance(document.querySelector("#editNoteModal"));
+    const followUpCommentModal = tailwind.Modal.getOrCreateInstance(document.querySelector("#followUpCommentModal"));
 
     let addEditor;
     if($("#addEditor").length > 0){
@@ -329,6 +338,13 @@ var studentNotesListTable = (function () {
 
         editEditor.setData('');
         edit_note_term_declaration_id.clear(true);
+    });
+
+    const followUpCommentModalEl = document.getElementById('followUpCommentModal')
+    followUpCommentModalEl.addEventListener('hide.tw.modal', function(event) {
+        $('#followUpCommentModal textarea').val('');
+        $('#followUpCommentModal [name="student_note_id"]').val('0');
+        $('#followUpCommentModal #followUpCommentWrap').html('Loading...');
     });
 
     const viewNoteModalEl = document.getElementById('viewNoteModal')
@@ -751,6 +767,32 @@ var studentNotesListTable = (function () {
             }).catch(error =>{
                 console.log(error)
             });
+        }else if(action == 'COMPLETEFOLLOWUP'){
+            axios({
+                method: 'POST',
+                url: route('followups.completed'),
+                data: {recordid : recordid},
+                headers: {'X-CSRF-TOKEN' :  $('meta[name="csrf-token"]').attr('content')},
+            }).then(response => {
+                if (response.status == 200) {
+                    $('#confirmModal button').removeAttr('disabled');
+                    confirmModal.hide();
+                    studentNotesListTable.init();
+
+                    successModal.show();
+                    document.getElementById('successModal').addEventListener('shown.tw.modal', function(event){
+                        $('#successModal .successModalTitle').html('Done!');
+                        $('#successModal .successModalDesc').html('The process successfully completed.');
+                        $('#successModal .successCloser').attr('data-action', 'NONE');
+                    });
+
+                    setTimeout(function(){
+                        successModal.hide();
+                    }, 2000);
+                }
+            }).catch(error =>{
+                console.log(error)
+            });
         }else{
             confirmModal.hide();
         }
@@ -812,6 +854,114 @@ var studentNotesListTable = (function () {
                 console.log('error');
             }
         });
+    });
+
+    $('#studentNotesListTable').on('click', '.completedBtn', function(e){
+        e.preventDefault();
+        var $btn = $(this);
+        var noteId = $btn.attr('data-id');
+
+        confirmModal.show();
+        document.getElementById("confirmModal").addEventListener("shown.tw.modal", function (event) {
+            $("#confirmModal .confModTitle").html('Select "Yes" to finish the process.' );
+            $("#confirmModal .agreeWith").attr('data-recordid', noteId);
+            $("#confirmModal .agreeWith").attr('data-status', 'COMPLETEFOLLOWUP');
+        });
+    });
+
+    $('#studentNotesListTable').on('click', '.viewCommentBtn', function(e){
+        e.preventDefault();
+        var $btn = $(this);
+        var note_id = $btn.attr('data-id');
+        axios({
+            method: "POST",
+            url: route('followups.comment.list'),
+            data: {note_id : note_id},
+            headers: {'X-CSRF-TOKEN' :  $('meta[name="csrf-token"]').attr('content')},
+        }).then(response => {
+            if (response.status == 200) {
+                $('#followUpCommentWrap').html(response.data.htm);
+                $('#followUpCommentModal [name="student_note_id"]').val(note_id);
+
+                
+                setTimeout(() => {
+                    $('#followUpCommentModal').find('.tooltip').each(function () {
+                        let tippyOption = {
+                            content: $(this).attr("alt"),
+                        };
+                        tippy(this, {
+                            arrow: roundArrow,
+                            animation: "shift-away",
+                            zIndex: '9999999999',
+                            ...tippyOption,
+                        });
+                    });
+                }, 10);
+            }
+        }).catch(error => {
+            if (error.response) {
+                console.log('error');
+            }
+        });
+
+        studentNotesListTable.init();
+    });
+
+    $('#followUpCommentForm').on('submit', function(e){
+        e.preventDefault();
+        var $form = $(this);
+        const form = document.getElementById('followUpCommentForm');
+    
+        $('#postCommentBtn').attr('disabled', 'disabled');
+        $('#postCommentBtn svg.theIcon').fadeOut();
+        $('#postCommentBtn svg.theLoader').fadeIn();
+
+        if($('#the_comment', $form).val() != ''){
+            let form_data = new FormData(form);
+            axios({
+                method: "post",
+                url: route('followups.comment.store'),
+                data: form_data,
+                headers: {'X-CSRF-TOKEN' :  $('meta[name="csrf-token"]').attr('content')},
+            }).then(response => {
+                $('#postCommentBtn').attr('disabled', 'disabled');
+                $('#postCommentBtn svg.theIcon').fadeIn();
+                $('#postCommentBtn svg.theLoader').fadeOut();
+
+                if (response.status == 200) {
+                    $('#followUpCommentWrap').html(response.data.htm);
+                    $('#the_comment', $form).val('');
+
+                
+                    setTimeout(() => {
+                        $('#followUpCommentModal').find('.tooltip').each(function () {
+                            let tippyOption = {
+                                content: $(this).attr("alt"),
+                            };
+                            tippy(this, {
+                                arrow: roundArrow,
+                                animation: "shift-away",
+                                zIndex: '9999999999',
+                                ...tippyOption,
+                            });
+                        });
+                    }, 10);
+                }
+            }).catch(error => {
+                $('#postCommentBtn').attr('disabled', 'disabled');
+                $('#postCommentBtn svg.theIcon').fadeIn();
+                $('#postCommentBtn svg.theLoader').fadeOut();
+                if (error.response) {
+                    console.log('error');
+                }
+            });
+        }else{
+            $('#postCommentBtn').attr('disabled', 'disabled');
+            $('#postCommentBtn svg.theIcon').fadeIn();
+            $('#postCommentBtn svg.theLoader').fadeOut();
+        }
+
+        studentNotesListTable.init();
     });
 
 
