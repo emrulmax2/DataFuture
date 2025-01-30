656000ced7b6173ead7a8f8f63c3629f640b393f
Emrul Easir <emrulmax2@gmail.com>
2025-01-30 19:57:20 +0600

    Update StudentDataReportController.php

--- app/Http/Controllers/Reports/StudentDataReportController.php
+++ app/Http/Controllers/Reports/StudentDataReportController.php
@@ -266,7 +266,7 @@ class StudentDataReportController extends Controller
         
        
 
-        $StudentData = Student::with('other','crel','termStatus','termStatusLatest','course','award','nation','contact','kin','disability','quals','status','ProofOfIdLatest','qualHigest','qualHigest.previous_providers','qualHigest.qualification_type_identifiers','slcAgreement')->whereIn('id',$studentIds)->get();
+        $StudentData = Student::with('other','termStatus','termStatusLatest','course','award','nation','contact','kin','disability','quals','status','ProofOfIdLatest','qualHigest','qualHigest.previous_providers','qualHigest.qualification_type_identifiers','slcAgreement')->whereIn('id',$studentIds)->get();
 
         
 
