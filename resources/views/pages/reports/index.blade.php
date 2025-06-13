59e55fb32ce2722ea038722697b373ab1019af06
Emrul Easir <emrulmax2@gmail.com>
2025-06-13 22:05:07 +0600

    Update index.blade.php

--- resources/views/pages/reports/index.blade.php
+++ resources/views/pages/reports/index.blade.php
@@ -59,6 +59,7 @@
                 </a>
             </div>
             
+            
             <div class="col-span-12 sm:col-span-3 xl:col-span-2 2xl:col-span-1">
                 <a href="{{ route('report.student.progress.view'); }}" class="box introy-y zoom-in bg-primary flex justify-center items-center">
                     <img class="block w-full h-auto shadow-md zoom-in rounded" alt="Student Progress Report" src="{{ asset('build/assets/images/report_icons/student_progress_report.png') }}">
@@ -75,6 +76,13 @@
             <div class="col-span-12 sm:col-span-3 xl:col-span-2 2xl:col-span-1">
                 <a href="{{ route('report.student.workplacement.view'); }}" class="box introy-y zoom-in bg-primary flex justify-center items-center">
                     <img class="block w-full h-auto shadow-md zoom-in rounded" alt="Student Workplacement Report" src="{{ asset('build/assets/images/report_icons/student-workplacement-reports.png') }}">
+                </a>
+            </div>
+
+            
+            <div class="col-span-12 sm:col-span-3 xl:col-span-2 2xl:col-span-1">
+                <a href="{{ route('report.student.expected.result.view'); }}" class="box introy-y zoom-in bg-primary flex justify-center items-center">
+                    <img class="block w-full h-auto shadow-md zoom-in rounded" alt="Student Expected Result Report" src="{{ asset('build/assets/images/report_icons/student_expected_report.png') }}">
                 </a>
             </div>
             
