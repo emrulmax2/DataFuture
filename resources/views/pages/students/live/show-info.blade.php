035c96546a04e120fc6283b442fcd73059c74cbf
Emrul Easir <emrulmax2@gmail.com>
2025-08-28 22:27:12 +0600

    Update show-info.blade.php

--- resources/views/pages/students/live/show-info.blade.php
+++ resources/views/pages/students/live/show-info.blade.php
@@ -37,21 +37,39 @@
                 <div class="dropdown-menu w-40">
                     <ul class="dropdown-content">
                         @if(isset($student->children) && count($student->children) > 0)
-                           @foreach($student->descendants as $descendant)
-                               <li>
-                                   <a href="{{ route('student.show', $descendant->id) }}" class="dropdown-item">
-                                       <i data-lucide="user" class="w-4 h-4 mr-2"></i> View {{ $descendant->course->semester->name }}
-                                   </a>
-                               </li>
-                           @endforeach
+                            @if(isset($student->descendants))
+                                @foreach($student->descendants as $descendant)
+                                    <li>
+                                        <a href="{{ route('student.show', $descendant->id) }}" class="dropdown-item">
+                                            <i data-lucide="user" class="w-4 h-4 mr-2"></i> View {{ $descendant->course->semester->name }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            @else
+                                @foreach($student->children as $child)
+                                    <li>
+                                        <a href="{{ route('student.show', $child->id) }}" class="dropdown-item">
+                                            <i data-lucide="user" class="w-4 h-4 mr-2"></i> View {{ $child->course->semester->name }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            @endif
                         @elseif(isset($student->parent))
-                            @foreach($student->ancestors as $ancestor)
+                            @if(isset($student->ancestors))
+                                @foreach($student->ancestors as $ancestor)
+                                    <li>
+                                        <a href="{{ route('student.show', $ancestor->id) }}" class="dropdown-item">
+                                            <i data-lucide="user" class="w-4 h-4 mr-2"></i> View {{ $ancestor->course->semester->name }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            @else
                                 <li>
-                                    <a href="{{ route('student.show', $ancestor->id) }}" class="dropdown-item">
-                                        <i data-lucide="user" class="w-4 h-4 mr-2"></i> View {{ $ancestor->course->semester->name }}
+                                    <a href="{{ route('student.show', $student->parent->id) }}" class="dropdown-item">
+                                        <i data-lucide="user" class="w-4 h-4 mr-2"></i> View {{ $student->parent->course->semester->name }}
                                     </a>
                                 </li>
-                            @endforeach
+                            @endif
                         @else
                             <li>
                                 <span class="dropdown-item">
