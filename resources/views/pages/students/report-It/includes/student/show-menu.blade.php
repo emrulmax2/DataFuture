e36e59bd4016040f6c2d155e9083e647306293e8
Emrul Easir <emrulmax2@gmail.com>
2025-10-02 07:36:19 +0600

    remove data

--- resources/views/pages/students/report-It/includes/student/show-menu.blade.php
+++
@@ -1,9 +0,0 @@
-
-<ul class="nav nav-link-tabs flex-col sm:flex-row justify-center lg:justify-start text-center liveStudentProfileMainMenu">
-    <li class="nav-item" role="presentation">
-        <a href="" class="nav-link py-4 active">
-            Detail Information
-        </a>
-    </li>
-    
-</ul>
