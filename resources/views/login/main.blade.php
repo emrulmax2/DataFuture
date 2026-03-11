e794a4f7e7cb58410f08782e2539a9532189c0e8
Emrul Easir <emrulmax2@gmail.com>
2026-03-12 04:24:58 +0600

    Update main.blade.php

--- resources/views/login/main.blade.php
+++ resources/views/login/main.blade.php
@@ -159,7 +159,6 @@
                                 <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
                                 <label class="cursor-pointer select-none" for="remember-me">Remember me</label>
                             </div>
-                            
                         </div>
                         <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
                             <button id="btn-login" class="btn btn-primary py-3 px-4 w-full md:w-64 xl:mr-3 align-top">Staff Login</button>
