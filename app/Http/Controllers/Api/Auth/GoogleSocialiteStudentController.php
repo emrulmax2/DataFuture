8269eef1c4ad1b6b83c06e1c01f8285b08f73a1c
Emrul Easir <emrulmax2@gmail.com>
2026-08-17 15:36:22 +0600

    Update GoogleSocialiteStudentController.php

--- app/Http/Controllers/Api/Auth/GoogleSocialiteStudentController.php
+++ app/Http/Controllers/Api/Auth/GoogleSocialiteStudentController.php
@@ -26,6 +26,7 @@ class GoogleSocialiteStudentController extends Controller
         $request->validate([
             'id_token' => 'required|string',
         ]);
+        dd($request->all());
         try {
 
             $client = new GoogleClient([
